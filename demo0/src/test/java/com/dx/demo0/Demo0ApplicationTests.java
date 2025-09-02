5cbd467c836d2056bd25db496b86f223abcddb9a
zhourui <1572729241@qq.com>
2025-09-02 15:53:41 +0800

    demo0

---
+++ demo0/src/test/java/com/dx/demo0/Demo0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dx.demo0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo0ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
