b3ca8d9e4ab8fc0d99fdfc16a077b01b26e3f17d
zhourui <1572729241@qq.com>
2025-09-02 16:35:00 +0800

    依赖

--- demo0/src/main/java/com/dx/demo0/controller/Hello.java
+++ demo0/src/main/java/com/dx/demo0/controller/Hello.java
@@ -1,13 +1,16 @@
 package com.dx.demo0.controller;
 
 
+import com.dx.demo0.entity.HelloResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
 public class Hello {
     @GetMapping("/")
-    public String hello() {
-        return "hello";
+    public HelloResponse hello() {
+        return new HelloResponse("hello");
     }
 }
